4e727a41a2b993f6c31e4d66bb4d4fa2ce86eed9
Lauritha <lizamavergaralaura@gmail.com>
2016-07-02 01:37:34 -0400

    boton agregar tarea y limpiar funcionales, se crea el div en la linea siguiente quedando vacio el textarea pero falta editar

--- js/main.js
+++ js/main.js
@@ -7,25 +7,29 @@ function agregarTarea() {
    
     if(texto != ""){
     	var div = document.createElement("div");
+        var btn = document.createElement("button");
+        btn.innerHTML = "eliminar";
+        btn.addEventListener('click', limpiarTarea);
 		listaTarea.appendChild(div);
 		div.innerHTML = texto;
-		inputText.innerHTML = "";
+        div.appendChild(btn);
+		document.getElementById("inputText").value = "";
     }else{
-    	alert("No es posible ingresar la tarea, debe contener al menos 2 caracteres")
+    	alert("La tarea no puede ser ingresada")
     }
 }
+
 function limpiarTarea() {
     var texto = document.getElementById("inputText").value;
    
-    if(texto == ""){
-		div.innerHTML = texto;
-		inputText.innerHTML = "";
+    if(texto != ""){
+		document.getElementById("inputText").value = "";
     }else{
-    	alert("No es posible ingresar la tarea, debe contener al menos 2 caracteres")
+    	alert("No es posible limpiar el área de texto porque esta vacía")
     }
 }
-var clean = function() {
-	var inputBox = document.getElementById("inputBox");
-	inputBox.value = "";
-	inputBox.focus();
+function crearLista() {
+    var campoTarea = document.getElementById("campoTarea");
+    var tarea = document.createElement("div");
+    tarea.campoTarea.appendChild(div);
 }
