06ba2c80e35a4676dd7cd79f23acf7ea015e5e15
Lauritha <lizamavergaralaura@gmail.com>
2016-07-04 20:30:55 -0400

    trabajo terminado

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 var texto = document.getElementById("inputText").value;
 var listaTarea = document.getElementById("listaTarea");
 var inputText = document.getElementById("inputText");
+var campoTarea = document.getElementById("campoTarea");
 
 function agregarTarea() {
     var texto = document.getElementById("inputText").value;
-   
     if(texto != ""){
     	var div = document.createElement("div");
-        var btn = document.createElement("button");
-        btn.innerHTML = "eliminar";
-        btn.addEventListener('click', limpiarTarea);
-		listaTarea.appendChild(div);
-		div.innerHTML = texto;
+        div.setAttribute("class", "caja");
+        var contenido = document.createElement("span");
+        var check = document.createElement("input");
+        var btn = document.createElement("span");
+        btn.setAttribute("class", "fa fa-trash-o basurero");
+        check.setAttribute("type", "checkbox");
+        btn.addEventListener('click', borrarTarea);
+		check.onclick = function() {
+            if(this.checked == true){
+                contenido.setAttribute("class", "tachar");
+            } else {
+                contenido.removeAttribute("class");
+            }
+        }
+        listaTarea.appendChild(div);
+		contenido.innerHTML = texto;
+        div.appendChild(contenido);
+        div.appendChild(check);
         div.appendChild(btn);
 		document.getElementById("inputText").value = "";
     }else{
     	alert("La tarea no puede ser ingresada")
     }
+    
 }
 
 function limpiarTarea() {
@@ -32,4 +46,8 @@ function crearLista() {
     var campoTarea = document.getElementById("campoTarea");
     var tarea = document.createElement("div");
     tarea.campoTarea.appendChild(div);
+}
+function borrarTarea(e) {
+    var div = e.target.parentNode;
+    listaTarea.removeChild(div);
 }
